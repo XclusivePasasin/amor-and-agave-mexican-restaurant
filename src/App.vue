82c2607992f63ec2b40a38acec9e166c8c63938d
XclusivePasasin <elizar.rebollo23@itca.edu.sv>
2025-01-13 12:09:07 -0600

    Create and install vue, tailwind and flowbite

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="bg-green-500 text-white text-center p-4">
+    <h1 class="text-4xl font-bold">Prueba de Tailwind CSS</h1>
+    <p class="text-sm text-gray-600">Clase de texto en gris claro</p>
+  </div>
+</template>
+
+
+
+<style scoped>
+  
+</style>
